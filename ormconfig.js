56618667cb3b4db0ebad51d8a9adce3c2a969e29
Jaewon <j901207@gmail.com>
2021-05-05 14:21:16 +0900

    Add: signout API

---
+++ ormconfig.js
@@ -0,0 +1,21 @@
+import env from 'dotenv';
+env.config();
+
+module.exports = {
+  type: 'mysql',
+  host: process.env.DATABAES_ENDPOINT,
+  port: 3306,
+  username: 'root',
+  password: process.env.DATABASE_PW,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
